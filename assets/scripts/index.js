0c4e5d20290cbf339863329004734a1a0edf8aa7
Dan Dectis <dandectis@gmail.com>
2023-07-03 18:02:51 -0700

    dinner time update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,7 +17,7 @@ const healthUI = document.querySelector("#health")
 const stashUI = document.querySelector("#stash")
 
 
-let interestRate = 5; 
+let interestRate = 10; 
 let currentLocation = 0; //this is an array index value that connects to "locations" which is an array of location objects
 
 const welcomeMessage = `<p>Oh, man. <br/><br/>The county tax assessor 
@@ -49,14 +49,24 @@ With a big slap on the back, Tony says his guys will come find you in a week to 
 let statusMessage = "";
 
 
-let burroughs = {};
+let burroughs = {}; //this is an object that will contain additional info about each location e.g. services,  etc
 
+//define player stats placeholders
 let cash = 0;
 let debt = 0;
 let health = 0;
 let maxHealth = 0;
 let stashSize = 0;
 let countHeld = 0;
+let day = 0;
+
+//define travel button placeholders
+let manhattanButton = "";
+let harlemButton = "";
+let bronxButton = "";
+let brooklynButton = "";
+let queensButton = "";
+
 
 
 const printWelcomeMessage = () => {
@@ -70,14 +80,17 @@ const loadObjectsJSON = async() => {
         const objects = await response.json();
 
         burroughs = objects.locations; //grab the location objects from the objects master list
-        
+
+        console.log(burroughs)
 
         burroughs.forEach(burrough => {
-            console.log("This one is: " + burrough.name)
+            console.log("Createing Travel Button for: " + burrough.name)
             //create a button for each burrough
             travelButtonHolder.insertAdjacentHTML("beforeend",
-                `<div class="button" id="${burrough.name}-button">Travel to ${burrough.name}`)
+                `<div class="button" id="${burrough.name.toLowerCase()}-button">Travel to ${burrough.name}`)
+            
         })
+
         
         console.log(objects.playerInfo);
         //load the game variables from the json
@@ -86,15 +99,69 @@ const loadObjectsJSON = async() => {
         health = objects.playerInfo.maxHealth;
         maxHealth = objects.playerInfo.maxHealth;
         stashSize = objects.playerInfo.stashSize;
-        updateInfoPanelStats(); //and print their values
         
              
     } catch (error) {
         console.error("Error loading JSON file:", error);
     }
+
+    updateInfoPanelStats(); //and print their values
+    createTravelButtons();
     
 
 }
+
+//create references to the travel buttons and add listeners. Listeners should call a univeral travel function that takes a value in to point the travel destination
+const createTravelButtons = () => {
+    manhattanButton = document.querySelector("#manhattan-button")
+    manhattanButton.addEventListener("click", event => {
+        travelClick(0);
+        console.log("Manhattan click")
+    })
+    harlemButton = document.querySelector("#harlem-button")
+    harlemButton.addEventListener("click", event => {
+        travelClick(1);
+        console.log("Harlem Click");
+    })
+
+    bronxButton = document.querySelector("#bronx-button");
+    bronxButton.addEventListener("click", event => {
+        travelClick(2);
+        console.log("Bronx Click")
+    })
+
+    brooklynButton = document.querySelector("#brooklyn-button");
+    brooklynButton.addEventListener("click", event => {
+        travelClick(3)
+        console.log("Brooklyn Click")
+    })
+
+    queensButton = document.querySelector("#queens-button");
+    queensButton.addEventListener("click", event => {
+        travelClick(4);
+        console.log("Queens Click")
+    })
+
+    console.log("travel button listeners added")
+    
+
+}
+
+const travelClick = destination => {
+    console.log("Traveling to : " + burroughs[destination].name);
+    currentLocation = destination;
+    day++;
+    let interest = debt * interestRate / 100
+    let trimmedInterest = parseFloat(interest.toFixed(2));
+    
+    debt += trimmedInterest; //make sure you are only adding numbers with 2 digits after the deci
+    console.log("Interest: " + interest);
+    updateStatusMessage();
+    updateInfoPanelStats();
+
+}
+
+
 
 
 //game start button click
@@ -116,10 +183,7 @@ continueFromIntroButton.addEventListener("click", event => {
 
     statusHeading.classList.remove("hide");
     //define the status message based on current burrough
-    let statusMessage = `<p>You are currently in ${burroughs[0].name}</p>`
-
-    //and print that message
-    statusMessageHolder.innerHTML = statusMessage;
+    updateStatusMessage();
 
     //and unhide the UI Info Panel
     infoPanel.classList.remove("hide");
@@ -132,15 +196,22 @@ continueFromIntroButton.addEventListener("click", event => {
 //call this method any time you want to update the printed UI info values
 const updateInfoPanelStats = () => {
     cashUI.textContent = `$${cash}`;
-    debtUI.textContent = `$${debt}`;
-    healthUI.textContent = `${health} / ${maxHealth} MAX`;
-    stashUI.textContent = `${countHeld} / ${stashSize} MAX`;
+    debtUI.textContent = `$${debt.toFixed(2)}`;
+    healthUI.textContent = `${health} / ${maxHealth}`;
+    stashUI.textContent = `${countHeld} / ${stashSize}`;
     console.log("UI values updated");
 
 }
 
+const updateStatusMessage = () => {
+    statusMessage = `<p>It is Day ${day}. You are currently in ${burroughs[currentLocation].name}</p>`
+    statusMessageHolder.innerHTML = statusMessage;
+}
 
 
+//update the objects json so that each of the locations specifies which activities are available
+//some activities will be available at all locations (dealing, traveling)
+//others will only be available at 1 or some (visit loan shark, visit the bank, weapons dealer, upgrade shop)
 
 
 
